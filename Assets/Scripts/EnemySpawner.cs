f645a9338e4a63f9d46cdf1c6b17c326e854d994
JessedK05 <jes.de.kok@sintlucasedu.nl>
2023-06-14 14:56:08 +0200

    EnemySpawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,16 +6,25 @@ public class EnemySpawner : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject[] enemyPrefabs;
+
+    public float Spawnrate = 1f;
+    private bool canSpawn = true;
     // Start is called before the first frame update
-    void Update()
+
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.M))
+        StartCoroutine(EnemySpawn());
+    }
+    public IEnumerator EnemySpawn()
+    {
+        WaitForSeconds Wait= new WaitForSeconds(Spawnrate);
+
+        while (canSpawn)
         {
+            yield return Wait;
             int randomEnemy = Random.Range(0, enemyPrefabs.Length);
             int randomSP = Random.Range(0, spawnPoints.Length);
             Instantiate(enemyPrefabs[randomEnemy], spawnPoints[randomSP].position, transform.rotation);
         }
-
-        
     }
 }
